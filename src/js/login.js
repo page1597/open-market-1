4add3e4be4b5016cfd25f2eaa2f366967191c524
page1597 <gimm08377@gmail.com>
2024-08-05 17:26:07 +0900

    Refactor: 돔 요소 선택 변수명 변경
    - 변수명 앞에 '$' 표시 추가
    - 가독성 향상 및 혼동 방지를 위함

--- src/js/login.js
+++ src/js/login.js
@@ -1,50 +1,49 @@
 const url = "https://openmarket.weniv.co.kr/";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("login-form");
-  const buyerButton = document.getElementById("buyer");
-  const sellerButton = document.getElementById("seller");
+  const $form = document.getElementById("login-form");
+  const $buyerButton = document.getElementById("buyer");
+  const $sellerButton = document.getElementById("seller");
 
-  form.dataset.loginType = "BUYER";
-  buyerButton.classList.add("active");
+  $form.dataset.loginType = "BUYER";
+  $buyerButton.classList.add("active");
 
-  buyerButton.addEventListener("click", () => {
+  $buyerButton.addEventListener("click", () => {
     setLoginType("BUYER");
   });
-  sellerButton.addEventListener("click", () => {
+  $sellerButton.addEventListener("click", () => {
     setLoginType("SELLER");
   });
 
   const setLoginType = (type) => {
-    form.dataset.loginType = type;
-    buyerButton.classList.toggle("active", type === "BUYER");
-    sellerButton.classList.toggle("active", type === "SELLER");
+    $form.dataset.loginType = type;
+    $buyerButton.classList.toggle("active", type === "BUYER");
+    $sellerButton.classList.toggle("active", type === "SELLER");
   };
 
-  form.addEventListener("submit", (e) => {
+  $form.addEventListener("submit", (e) => {
     e.preventDefault();
-    onSubmitForm(form);
+    onSubmitForm($form);
   });
 });
 
 const onSubmitForm = (form) => {
-  const loginButton = document.getElementById("login");
-  const idInput = document.getElementById("id");
-  const passwordInput = document.getElementById("password");
-  const errorMessage = document.getElementById("error-message");
+  const $idInput = document.getElementById("id");
+  const $passwordInput = document.getElementById("password");
+  const $errorMessage = document.getElementById("error-message");
 
-  const id = idInput.value.trim();
-  const password = passwordInput.value.trim();
+  const id = $idInput.value.trim();
+  const password = $passwordInput.value.trim();
 
   if (!id || !password) {
-    errorMessage.style.display = "block";
+    $errorMessage.style.display = "block";
 
     if (!id) {
-      errorMessage.textContent = "아이디를 입력해 주세요.";
-      idInput.focus();
+      $errorMessage.textContent = "아이디를 입력해 주세요.";
+      $idInput.focus();
     } else {
-      errorMessage.textContent = "비밀번호를 입력해 주세요.";
-      passwordInput.focus();
+      $errorMessage.textContent = "비밀번호를 입력해 주세요.";
+      $passwordInput.focus();
     }
     return;
   }
@@ -54,8 +53,8 @@ const onSubmitForm = (form) => {
 
 // 로그인
 const login = async (id, password, loginType) => {
-  const errorMessage = document.getElementById("error-message");
-  const passwordInput = document.getElementById("password");
+  const $errorMessage = document.getElementById("error-message");
+  const $passwordInput = document.getElementById("password");
   try {
     const res = await fetch(`${url}/accounts/login/`, {
       method: "POST",
@@ -73,15 +72,16 @@ const login = async (id, password, loginType) => {
 
     if (res.ok) {
       console.log("로그인 성공:", json);
-      errorMessage.style.display = "none";
+      $errorMessage.style.display = "none";
+      // token 저장
       window.location.href = "/";
       return true;
     } else {
       console.error("로그인 실패:", json);
-      passwordInput.focus();
-      passwordInput.value = "";
-      errorMessage.textContent = "아이디 또는 비밀번호가 일치하지 않습니다.";
-      errorMessage.style.display = "block";
+      $passwordInput.focus();
+      $passwordInput.value = "";
+      $errorMessage.textContent = "아이디 또는 비밀번호가 일치하지 않습니다.";
+      $errorMessage.style.display = "block";
       return false;
     }
   } catch (error) {
