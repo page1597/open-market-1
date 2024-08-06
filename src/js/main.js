d1e50da4afa0ca2bccef69c3df54df548d40735d
page1597 <gimm08377@gmail.com>
2024-08-06 14:41:21 +0900

    Refactor: header, footer를 별도의 HTML 파일로 분리
    - 공통 레이아웃인 header, footer를 별로의 HTML 파일로 분리하고 이를 JS를 통해 동적으로 로드

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,82 @@ const url = "https://openmarket.weniv.co.kr/";
 document.addEventListener("DOMContentLoaded", () => {
   const $productList = document.querySelector("product-list");
   console.log($productList);
+  displayProducts();
 });
+
+function getGroupRange(number) {
+  const groupStart = Math.floor((number - 1) / 10) * 10 + 1;
+  const groupEnd = groupStart + 9;
+
+  return [groupStart, groupEnd];
+}
+
+const createPaginationButtons = (totalPages) => {
+  const $pagination = document.querySelector(".pagination ul");
+  const $prevButton = document.getElementById("prev");
+  const $nextButton = document.getElementById("next");
+  const pageItems = Array.from($pagination.children).filter(
+    (li) => !li.querySelector("button")
+  );
+  pageItems.forEach((item) => $pagination.removeChild(item));
+
+  // 이전, 다음 버튼
+  const currentPage = parseInt(
+    new URLSearchParams(window.location.search).get("page") || 1,
+    10
+  );
+  let startPage = getGroupRange(currentPage)[0];
+  let endPage =
+    totalPages < getGroupRange(currentPage)[1]
+      ? totalPages
+      : getGroupRange(currentPage)[1];
+  console.log("startPage", startPage, "endPage", endPage);
+
+  if (totalPages > 1) {
+    const $nextButtonParent = $nextButton.parentElement;
+
+    $nextButton.onclick = () => {
+      let page = 1;
+      if (currentPage + 10 <= totalPages) {
+        page = currentPage + 10;
+      } else {
+        page =
+          totalPages < getGroupRange(currentPage + 10)[1]
+            ? totalPages
+            : getGroupRange(currentPage + 10)[1];
+      }
+      window.location.href = `/?page=${page}`; // 페이지 이동}
+      startPage += 10;
+      endPage += 10;
+    };
+
+    if (currentPage > 10) {
+      $prevButton.onclick = () => {
+        window.location.href = `/?page=${currentPage - 10}`;
+        startPage -= 10;
+        endPage -= 10;
+      };
+    } else {
+      $prevButton.disabled = true;
+    }
+
+    // 페이지 버튼 리스트 요소들
+    for (let i = startPage; i <= endPage; i++) {
+      const $li = document.createElement("li");
+      const $a = document.createElement("a");
+      $a.href = `/?page=${i}`;
+      $a.textContent = i;
+      $li.appendChild($a);
+
+      if (currentPage === i) {
+        $li.classList.add("current");
+      }
+
+      $pagination.insertBefore($li, $nextButtonParent);
+    }
+  }
+};
+
 // 상품 불러오기
 const fetchProducts = async () => {
   try {
